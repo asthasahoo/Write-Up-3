b1a604f8b8063fab344859dd2ad934662a260a38
asthasahoo <tennisastha16@gmail.com>
2016-09-25 13:26:28 -0700

    Update problem1.swift

--- problem1.swift
+++ problem1.swift
@@ -2,4 +2,10 @@
 
 let input1 = Int(readLine()!)!
 let input2 = Int(readLine()!)!
+var sum = input1
+var x = input1 + 1
+  while x < input2
+    sum = sum + x
+    x = x + 1
+  print("The sum of all the numbers between " + input1 + " and " + input2 + " is equal to " + (sum + input2) + ".")
 
